5ccadc8addb38185db1b1561b7bfb13852ea70b5
BrunoBaroncelli <80220011+BrunoBaroncelli@users.noreply.github.com>
2021-03-26 23:15:38 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -32,7 +32,7 @@ const Header = () => {
         <div>
           <BsPersonSquare
             onClick={() =>
-              (window.location.href = "https://react-video-lazarodu.vercel.app")
+              (window.location.href = "https://react-metalhead.vercel.app/")
             }
           />
         </div>
